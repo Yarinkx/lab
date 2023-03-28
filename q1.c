4b83e6c325fe15bf894814409807b92249ef02bb
yarin <yarinkx4@gmai.com>
2023-03-28 14:45:17 +0300

    fi

---
+++ q1.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+
+/*
+ * This program Performs 3 functions:
+
+  1) my_strcmp -This function reciving 2 string and calculates the difference in ASCII  between the  characters
+in the same index.
+
+ 2) my_strncmp -This function reciving 2 string and number of  characters for comparison .
+
+ 3) my_strchr -This function reciving a String and a character,then the function if the character
+found in string
+ If its exists It will return its index
+ if its not it will return a -1.
+
+ */
+int my_strcmp(char first[], char second[]);
+
+int my_strncmp(char first[], char second[], int n);
+
+int my_strchr(char str[], char c);
+
+
+int main() {
+    /*Variables for the function my_strcmp */
+    char firstCmp[80] = "";
+    char secondCmp[80] = "";
+
+    /*Variables for the function my_strncmp */
+    char firstNcmp[80] = "";
+    char secondNcmp[80] = "";
+    char c;
+
+    /*Variables for the function my_strchr */
+    char first[80] = "";
+    char r;
+    int result = 0;
+
+    printf("Test for my_strcmp: \n");
+    printf("Eneter 2 String: \n");
+    fgets(firstCmp, 80, stdin);
+    fgets(secondCmp, 80, stdin);
+    printf("\nThe difference is : %d \n", my_strcmp(firstCmp, secondCmp));
+
+    printf("\nTest for my_strncmp: ");
+    printf("\nEnter 2 Stirng: ");
+    fgets(firstNcmp, 80, stdin);
+    fgets(secondNcmp, 80, stdin);
+    printf("\nEnter how  many characters to check :");
+    c = (char) getchar();
+    printf("\nThe difference is : '%d' \n", my_strncmp(firstNcmp, secondNcmp, c));
+
+    printf("\nTest for my_strchr: ");
+    printf("\nEnter a string: ");
+    scanf("%s", first);
+    printf("\nEnter What character do u want to find in the string :\n");
+    getchar();
+    r = getchar();
+    result = my_strchr(first, r);
+    printf("\nThe index of the character in the string '%s' is :%d", first, result);
+
+}
+
+int my_strcmp(char first[], char second[]) {
+    int i = 0;
+    while (first[i] != '\0' && second[i] != '\0') { /*while loop that running As long as we haven't reached to the end of the strings*/
+        if (first[i] == second[i]) { /*if the character in the same location is the same character ,We will move on */
+            i++;
+        } else { /* If the condition is not happen We will return the difference between the characters*/
+            return first[i] - second[i];
+        }
+    }
+    return 0; /*If nothing happens we will return 0*/
+}
+
+int my_strncmp(char first[], char second[], int n) {
+    int i;
+    for (i = 0; i < n; i++) { /*for loop that running from to n (that is the amount of variables to test)*/
+        if (first[i] != second[i]) {/*if the character in the same location ist the same character ,We will return the difference  */
+            return first[i] - second[i];
+        }
+        if (second[i] == '\0' && first[i] == '\0') {/*If we have reached the end of the array for both we will return the difference */
+            return first[i] - second[i];
+        }
+    }
+    return 0;/*If nothing happens we will return 0*/
+}
+
+int my_strchr(char str[], char c) {
+    int i;
+    for (i = 0; str[i] != '\0'; i++) {/*for loop that going from the beginning of the string to the end of it*/
+        if (str[i] ==c) {/* Conditions that checks if the character we wanted is in a certain location in the string,If so,we will return its location*/
+            return i;
+        }
+    }
+    if (c == '\0') {
+        return i;
+    }
+    return -1;/*If nothing happens we will return -1*/
+}
---
+++ q2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/*
+ * A function called "bits_count" which summarizes and returns the number of bits that are
+in even places
+ */
+
+int count_bits(unsigned long x);
+
+int  main(){
+    unsigned long i = 0;
+    int x=0;
+        printf("\nPlease enter a  number: ");
+        scanf("%i",&i);
+        printf("\nthe even bits in the number:%i\n is :%d\n",i,count_bits(i));
+    }
+
+int count_bits(unsigned long x){
+
+    int count=0;
+    int even=0;
+    unsigned int mask=1;
+    while (mask){/*While loop   mask until mask different from zero */
+        if((x&mask)&&(even%2==0)){/*Checks how much the bit is worth and if its a Even number,Then we will count it*/
+            count++;
+        }
+        even++;/*else we will continue to the next Bit*/
+        mask=mask<<1;
+    }
+    return count;/*returns the number of bits that are in even places*/
+}
